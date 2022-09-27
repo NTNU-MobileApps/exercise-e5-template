eb7704defb9149038b951507ceb98d0f8708fcab
Girts Strazdins <girts.strazdins@gmail.com>
2022-09-27 19:03:57 +0200

    (WIP) Add Flutter-app skeleton, not working yet

---
+++ lib/book_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
