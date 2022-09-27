6e089a03bc7d7f2b500fedcf796ae330b39d5022
Girts Strazdins <girts.strazdins@gmail.com>
2022-09-27 17:43:30 +0200

    Refactor tests for sleepMilliseconds()

--- test/step_1_2_test.dart
+++ test/step_1_2_test.dart
@@ -6,11 +6,15 @@ import 'test_utilities.dart';
 /// Tests Part 1.2: the sleepMilliseconds() function
 void main() {
   test('Step 2: Millisecond sleep', () async {
-    final int timeAtStart = getSystemTimeMs();
-    await sleepMilliseconds(300);
-    final int timeInTheEnd = getSystemTimeMs();
-    final double longDuration = timeDifferenceMs(timeAtStart, timeInTheEnd);
-    const expectedLongOperationTime = 0.3;
-    expect(longDuration, closeTo(expectedLongOperationTime, 0.05));
+    await testSleeping(300);
+    await testSleeping(200);
   });
+}
+
+Future<void> testSleeping(int milliseconds) async {
+  final int timeBefore = getSystemTimeMs();
+  await sleepMilliseconds(milliseconds);
+  final int timeAfter = getSystemTimeMs();
+  expect(timeDifferenceMs(timeBefore, timeAfter),
+      closeTo(milliseconds / 1000.0, 0.05));
 }
