6bad8680a6e5f11a242e850fc6f5a7c9ea989699
Girts Strazdins <girts.strazdins@gmail.com>
2022-09-27 22:17:16 +0200

    Template code for Part 3: StreamBuilder

--- lib/book_app.dart
+++ lib/book_app.dart
@@ -1,13 +1,52 @@
+import 'package:exercise_e5/infrastructure/book_repository.dart';
 import 'package:flutter/material.dart';
+
+import 'infrastructure/book.dart';
 
 void main() {
   runApp(BookApp());
 }
 
+/// An app showing books from a library (imaginary database)
 class BookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Book app")),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // TODO - change this widget so that the BookItem widgets are
+              //  created based on the Book objects received from
+              //  the BookService.getBooks() stream!
+              BookItem(spectorBook),
+              BookItem(spectorBook),
+              BookItem(spectorBook),
+              BookItem(spectorBook)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// A widget displaying a book as an item in a list
+class BookItem extends StatelessWidget {
+  final Book book;
+
+  BookItem(this.book);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(child: Text(book.title)),
+      ),
+    );
   }
 }
